6cc002d566a07de6a57eb9985783fba377573ed1
Sunny-2610 <sunnysinha9884@gmail.com>
2026-03-06 14:30:01 +0530

    did something of the post

--- app/routes/issues.py
+++ app/routes/issues.py
@@ -1,18 +1,19 @@
 import uuid
 from fastapi import APIRouter, HTTPException, status
-from app.schemas import IssueCreate , IssueOut ,IssueUpdate
-from app.storage  import load_data , save_data
+from app.schemas import IssueCreate, IssueOut, IssueUpdate
+from app.storage import load_data, save_data
 
 
 router = APIRouter(prefix="/api/v1/issues", tags=["Issues"])
 
-@router.get("/" ,response_model=list[IssueOut])
+
+@router.get("/", response_model=list[IssueOut])
 async def get_issues():
     issues = load_data()
     return issues   
 
 
-    @router.post("", response_model=IssueOut, status_code=status.HTTP_201_CREATED)
+@router.post("", response_model=IssueOut, status_code=status.HTTP_201_CREATED)
 def create_issue(payload: IssueCreate):
     """
     Create new issue
