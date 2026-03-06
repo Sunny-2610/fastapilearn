11f4507dc0d95d73a7264c8acc4ba3bcdf9db90c
Sunny-2610 <sunnysinha9884@gmail.com>
2026-03-06 13:50:10 +0530

    issue get route is done

--- app/routes/issues.py
+++ app/routes/issues.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter
+import uuid
+from fastapi import APIRouter, HTTPException, status
+from app.schemas import IssueCreate , IssueOut ,IssueUpdate
+from app.storage  import load_data , save_data
+
 
 router = APIRouter(prefix="/api/v1/issues", tags=["Issues"])
 
-@router.get("/")
+@router.get("/" ,response_model=list[IssueOut])
 async def get_issues():
-    return []
+    issues = load_data()
+    return issues   
